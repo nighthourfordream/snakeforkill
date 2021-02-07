b766a3ce4fdabe6930f5e7a69615c20da726868a
Ma'x <78306210+nighthourfordream@users.noreply.github.com>
2021-02-07 19:51:37 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Snakeit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+          
+
+
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow(); 
+        
+    
+        }
+
+    }
+}
---
+++ VerticalLines.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snakeit
+{
+    class VerticalLines : Figure
+    {
+       
+
+        public VerticalLines( int yUp, int yDown, int x, char sym )
+        {
+            pList = new List<Point>();
+            for(int y = yUp; y <= yDown; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+
+    }
+}
